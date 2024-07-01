8fc031e6d84353c86f85b9621906f669cb1de80e
ashiqee <ashiq.buet73@gmail.com>
2024-07-01 10:31:15 +0600

    update todo

--- src/components/todo/TodoCard.tsx
+++ src/components/todo/TodoCard.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import { Button } from '../ui/button';
 
 const TodoCard = () => {
     return (
-        <div className="bg-slate-50  rounded-md flex justify-between items-center p-3">
+        <div className="bg-slate-50 border gap-4 rounded-md flex justify-between items-center p-3">
         <input type="checkbox" />
         <p>Todo title</p>
         <p>Time</p>
 
         <p>Description</p>
 
-        <div>
-          <button>del</button>
-          <button>edit</button>
+        <div className='flex gap-1 '>
+          <Button>del</Button>
+          <Button>edit</Button>
+          
         </div>
       </div>
     );
--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -6,16 +6,19 @@ import TodoCard from "./TodoCard";
 const TodoContainer = () => {
   return (
     <div>
-      <div>
+      <div className="flex justify-between items-center">
         <Button>Add Todo</Button>
         <Button>Filter</Button>
       </div>
-      <div className=" w-full h-full p-5 space-y-3 bg-primary-gradient rounded-xl my-5  mx-auto px-4 sm:px-6 lg:px-8">
+      <div className=" w-full h-full p-[5px] space-y-3 bg-primary-gradient rounded-xl my-5  mx-auto ">
       
-      <div className="bg-white p-3 flex justify-center items-center rounded-md text-2xl font-bold">
-       <p> There is no Todo</p>
-        </div>
+      <div className="bg-white p-3 space-y-3  text-center rounded-md ">
+       {/* <p className="text-2xl"> There is no Todo</p> */}
+       <TodoCard/>
       <TodoCard/>
+      <TodoCard/>
+        </div>
+  
      
       </div>
     </div>
